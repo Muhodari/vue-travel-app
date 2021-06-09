706e5b03837fec37c260c24103343dc8e02a48ff
Muhodari <sagemuho@gmail.com>
2021-06-09 21:51:26 +0200

    final deploy

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-
 Vue.config.productionTip =false;
 
 new Vue({
   router,
-  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
